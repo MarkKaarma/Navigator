5bc0bcdb22aaad6b38232e1b268d55d0df139d80
opilane <opilane@HP-CZC6297BBB>
2020-11-17 13:15:27 +0200

    add new project - "Ajaplaan".

--- Navigator.Android/Resources/Resource.designer.cs
+++ Navigator.Android/Resources/Resource.designer.cs
@@ -8376,19 +8376,25 @@ namespace Navigator.Droid
 			public const int notify_panel_notification_icon_bg = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int pepe_cross = 2131165307;
+			public const int nuli = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int terjer = 2131165308;
+			public const int pepe_cross = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tooltip_frame_dark = 2131165309;
+			public const int pepe_sad = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_light = 2131165310;
+			public const int terjer = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int uippet = 2131165311;
+			public const int tooltip_frame_dark = 2131165311;
+			
+			// aapt resource value: 0x7F070080
+			public const int tooltip_frame_light = 2131165312;
+			
+			// aapt resource value: 0x7F070081
+			public const int uippet = 2131165313;
 			
 			static Drawable()
 			{
---
+++ Navigator/Dopolnenie/Ajaplaan.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigator.Dopolnenie
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ajaplaan : ContentPage
+    {
+        public Ajaplaan()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+    private void TimetoChange(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
+        {
+            var time = TimePicker.Time.Hours;
+            if (time == 1)
+            {
+                Labla.Text = "Ночь... Красиво";
+                img = "nuli.jpg";
+            }
+        }
+    }
+}
